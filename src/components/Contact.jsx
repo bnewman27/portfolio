a71552f33a18780923cab1b2413227dc5053aa60
Ben McIntyre <ben@Bens-MBP.fios-router.home>
2023-03-26 11:34:17 -0700

    Updates to CSS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,39 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.scss';
 
 const Contact = () => {
-    return (
-        <>
-            <nav className={styles.nav}>
-                <div className={styles.navContainer}>
-                    <Link to="/" className={styles.navLink}>
-                        <button className={styles.navButton}>
-                            <p>Home</p>
-                        </button>
-                    </Link>
-                    <Link to="/about" className={styles.navLink}>
-                        <button className={styles.navButton}>
-                            <p>About</p>
-                        </button>
-                    </Link>
-                    <Link to="/projects" className={styles.navLink}>
-                        <button className={styles.navButton}>
-                            <p>Projects</p>
-                        </button>
-                    </Link>
-                    <Link to="/contact" className={styles.navLink}>
-                        <button className={styles.navButton}>
-                            <p>Contact</p>
-                        </button>
-                    </Link>
-                </div>
-            </nav>
-            <div className={styles.aboutContainer}>
-                <h1>Contact Page</h1>
-            </div>
-        </>
-    );
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 1000);
+  }, []);
+
+  return (
+      <div
+        className={`${styles['container']} ${
+          isLoaded ? styles.visible : ''
+        }`}
+      >
+        <h1 className={styles['header']}>
+          Contact
+        </h1>
+      </div>
+  );
 };
 
 export default Contact;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import styles from '../styles/Home.module.scss';
 
 const Home = () => {
@@ -12,45 +11,15 @@ const Home = () => {
   }, []);
 
   return (
-    <>
-      <nav
-        className={`${styles.nav} ${
-          isLoaded ? styles.show : ''
-        }`}
-      >
-        <div className={styles.navContainer}>
-          <Link to="/" className={styles.navLink}>
-            <button className={styles.navButton}>
-              <p>Home</p>
-            </button>
-          </Link>
-          <Link to="/about" className={styles.navLink}>
-            <button className={styles.navButton}>
-              <p>About</p>
-            </button>
-          </Link>
-          <Link to="/projects" className={styles.navLink}>
-            <button className={styles.navButton}>
-              <p>Projects</p>
-            </button>
-          </Link>
-          <Link to="/contact" className={styles.navLink}>
-            <button className={styles.navButton}>
-              <p>Contact</p>
-            </button>
-          </Link>
-        </div>
-      </nav>
       <div
-        className={`${styles['home-container']} ${
+        className={`${styles['container']} ${
           isLoaded ? styles.visible : ''
         }`}
       >
-        <h1 className={styles['home-header']}>
-          Home Page
+        <h1 className={styles['header']}>
+          Home
         </h1>
       </div>
-    </>
   );
 };
 
